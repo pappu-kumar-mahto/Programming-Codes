69ecf7ed9b2db1f40db82ae15d2a42d45fca6c96
Pappu Kumar Mahto <pappukumar.mahto@gmail.com>
2022-02-27 15:24:14 +0530

    Valid Square Problem updated

--- SquareRoot.java
+++ SquareRoot.java
@@ -17,4 +17,24 @@ public class SquareRoot {
         }
         return -1;
     }
+    public boolean isPerfectSquare(int num) {
+        int start =0; 
+        int end = num;
+        long ans = -1;
+        while(start <= end){
+        long mid = start + (end - start)/2;
+        long square = mid * mid;
+        if(num >= square){
+            ans = mid;
+            start = (int) mid + 1;
+        }else{
+            end = (int) mid -1;
+            }
+        }
+        if(ans * ans == num){
+            return true;
+        }
+        return false;
+        
+    }
 }
